a344824f05122a4dcd2b919a8684eed9d9b3a692
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 14:20:08 +0100

    added course data

---
+++ fs-2025-api-20250925-/Data/CourseData.cs
@@ -0,0 +1,23 @@
+﻿using fs_2025_api_20250925_.Models;
+using System.Text.Json;
+
+namespace fs_2025_api_20250925_.Data
+{
+    public class CourseData
+    {
+        public List<Course> Courses { get; private set; } = new List<Course>();
+
+        public CourseData()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            string filePath = Path.Combine(AppContext.BaseDirectory, "Data", "coursedata.json");
+            var jsonData = File.ReadAllText(filePath);
+            Courses = JsonSerializer.Deserialize<List<Course>>(jsonData, options) ?? new List<Course>();
+        }
+
+    }
+}
