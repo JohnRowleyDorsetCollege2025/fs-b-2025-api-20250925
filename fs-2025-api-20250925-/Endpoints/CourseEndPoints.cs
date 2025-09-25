b9ded182260387bf220bd7222ff5619f3e49de22
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 14:35:02 +0100

    aded course endpoint

--- fs-2025-api-20250925-/Endpoints/CourseEndPoints.cs
+++ fs-2025-api-20250925-/Endpoints/CourseEndPoints.cs
@@ -1,10 +1,20 @@
-﻿namespace fs_2025_api_20250925_.Endpoints
+﻿using fs_2025_api_20250925_.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace fs_2025_api_20250925_.Endpoints
 {
     public static class CourseEndPoints
     {
         public static void AddCourseEndPoints(this WebApplication app)
         {
-         //   app.MapGet("/courses", ()=> ) 
+            app.MapGet("/courses", LoadAllCoursesAsync); 
+        }
+
+        private static async Task<IResult> LoadAllCoursesAsync(CourseData courseData)
+        {
+            var output = courseData.Courses;
+            return Results.Ok(output);
         }
     }
 }
+
--- fs-2025-api-20250925-/Program.cs
+++ fs-2025-api-20250925-/Program.cs
@@ -9,7 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.AddDependencies();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
