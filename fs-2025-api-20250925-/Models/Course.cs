03c916cd5a878cae792d0d768dcc1233eb816937
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-09-25 14:13:12 +0100

    added course class

---
+++ fs-2025-api-20250925-/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿namespace fs_2025_api_20250925_.Models
+{
+
+    public class Course
+    {
+        public int id { get; set; }
+        public bool isPreorder { get; set; }
+        public string courseUrl { get; set; }
+        public string courseType { get; set; }
+        public string courseName { get; set; }
+        public int courseLessonCount { get; set; }
+        public decimal courseLengthInHours { get; set; }
+        public string shortDescription { get; set; }
+        public string courseImage { get; set; }
+        public int priceInUSD { get; set; }
+        public string coursePreviewLink { get; set; }
+    }
+
+}
